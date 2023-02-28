a40b11558ad4c957f927b12396fc8ef147cbb335
MikayilovMuhammed <mahammad.mikayilov@code.edu.az>
2023-03-01 02:32:27 +0400

    Add first js codes

---
+++ js/index.js
@@ -0,0 +1,50 @@
+// String
+// Number
+// Boolean
+// Object
+// Null
+// Undefined
+// Symbol
+
+// var a = 5;
+
+// a = "salam";
+
+let b = "Salam P328";
+console.log("String value", b);
+
+b = 5.5;
+console.log("Number value", b);
+
+const c = 10;
+
+// c = "20";
+
+let isStudent = false;
+
+const arr = [5, 10, 1.1, "Salam", false, "salam"];
+
+const obj = {
+  name: "Muha",
+  surname: "Mikayilov",
+  isMarried: false,
+};
+
+console.log("Objects:", obj["isMarried"]);
+console.log("Objects:", obj.isMarried);
+
+obj.name = "Muhammed";
+obj.age = 23;
+
+// obj = { name: "s" };
+
+console.log(obj);
+
+console.log("Array:", arr);
+
+// let
+let d;
+
+console.log("Constant key", d);
+d = 5;
+console.log("Constant key - 2", d);
